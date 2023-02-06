68026d2c18f381970eaefcb61fa4bec11ff5db16
ArtZhevn <113771964+ArtZhevn@users.noreply.github.com>
2023-02-06 15:57:07 +0300

    step 6,7

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,11 +10,24 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('plain object', () => {
-  const expected = readFile('plain.txt');
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
+test.each([
+  ['file1.json', 'file2.json', 'stylish.txt'],
+  ['file1.yml', 'file2.yml', 'stylish.txt'],
+])('format stylish', (testFile1, testFile2, expectedFile) => {
+  const filepath1 = getFixturePath(testFile1);
+  const filepath2 = getFixturePath(testFile2);
   const actual = genDiff(filepath1, filepath2);
+  const expected = readFile(expectedFile);
+  expect(actual).toBe(expected);
+});
 
-  expect(actual).toEqual(expected);
+test.each([
+  ['file1.json', 'file2.json', 'plain.txt'],
+  ['file1.yml', 'file2.yml', 'plain.txt'],
+])('format plain', (testFile1, testFile2, expectedFile) => {
+  const filepath1 = getFixturePath(testFile1);
+  const filepath2 = getFixturePath(testFile2);
+  const actual = genDiff(filepath1, filepath2, 'plain');
+  const expected = readFile(expectedFile);
+  expect(actual).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
 import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
+import parse from './parsers.js';
+import format from './formatters/index.js';
 
-const readFile = (filepath) => {
+const getData = (filepath) => {
   const fullpath = path.resolve(process.cwd(), filepath);
   return fs.readFileSync(fullpath, 'utf-8');
 };
 
-const genDiff = (filepath1, filepath2) => {
-  const data1 = readFile(filepath1);
-  const object1 = JSON.parse(data1);
-  const keys1 = Object.keys(object1);
-
-  const data2 = readFile(filepath2);
-  const object2 = JSON.parse(data2);
-  const keys2 = Object.keys(object2);
-
+const getObject = (filepath) => {
+  const data = getData(filepath);
+  const extension = path.extname(filepath);
+  const object = parse(data, extension);
+  return object;
+};
+const buildTree = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2).sort();
-  const lines = keys.map((key) => {
-    if (!Object.hasOwn(object1, key)) {
-      return `  + ${key}: ${object2[key]}`;
-    }
-    if (!Object.hasOwn(object2, key)) {
-      return `  - ${key}: ${object1[key]}`;
-    }
-    if (object1[key] !== object2[key]) {
-      return `  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`;
+  const result = keys.map((key) => {
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return { key, type: 'nested', children: buildTree(obj1[key], obj2[key]) };
     }
 
-    return `    ${key}: ${object1[key]}`;
+    if (!Object.hasOwn(obj1, key)) {
+      return { key, type: 'added', value: obj2[key] };
+    }
+    if (!Object.hasOwn(obj2, key)) {
+      return { key, type: 'deleted', value: obj1[key] };
+    }
+    if (obj1[key] !== obj2[key]) {
+      return {
+        key, type: 'changed', valueBefore: obj1[key], valueAfter: obj2[key],
+      };
+    }
+    return { key, type: 'unchanged', value: obj1[key] };
   });
+  return result;
+};
 
-  return ['{', ...lines, '}'].join('\n');
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const object1 = getObject(filepath1);
+  const object2 = getObject(filepath2);
+  const diff = buildTree(object1, object2);
+
+  return format(diff, formatName);
 };
 
 export default genDiff;
